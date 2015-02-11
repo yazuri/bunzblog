7b36c25bcab6d13f7a435a323b8e44a70f6404b5
Bunyamin Ersoy <bunyamin.ersoy@hotmail.com>
2015-02-11 18:16:27 +0100

    Blog bunz
    MVC test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,17 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(
+			title: params[:article][:title],
+			body: params[:article][:body])
+		@article.save
+		redirect_to article_path(@article)
+	end
 
 
 end
